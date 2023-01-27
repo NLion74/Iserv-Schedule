f8a18e5c1ab640b25f5ec098dc68cfa1b8d8a90c
NLion74 <97677226+NLion74@users.noreply.github.com>
2023-01-28 00:18:08 +0100

    Added Matrix-Notifier to available notifications

--- iserv-schedule.py
+++ iserv-schedule.py
@@ -5,13 +5,18 @@ import requests
 import pandas as pd
 from discord_webhook import DiscordWebhook
 
-username = "user_name"
-password = "user_pass" # You will have to disable 2fA
-domain = "https://your.iserv.instance"
-your_class = "class"
+username = "jonte.peiss"
+password = "2ACiaZ68QywTbb" # You will have to disable 2fA
+domain = "https://igs-buxtehude.de"
+your_class = "nan"
 
-notify_method = "discord_webhook"
-webhook_url = "https//webhook_url" # Optional only required when using discord webhook notify_method
+notify_method = ["discord_webhook", "matrix_notifier"] # Available options: discord_webhook, matrix_notifier
+
+# Discord Webhook
+webhook_url = "https://discord.com/api/webhooks/1067770612031946762/g4xappOgKstKJnoC08oer2Yt9VMJ0D5kfNpcc_xM1QzDC6tQ1ZTzeLS1Z3ByIwsu6swr" # Optional only required when using discord webhook notify_method
+
+# Matrix-Notifier
+url = "http://127.0.0.1:5505"
 
 paths = {
         "login": "/iserv/auth/login?_target_path=/iserv/auth/auth?_iserv_app_url%3D%2Fiserv%2F%26client_id%3D16_6cic5kw2maskwckgg804kg400w8wkwwc4o484koswsgsk40okw%26nonce%3D334a68be-3900-4304-ae1d-ad6a97de420d%26redirect_uri%3Dhttps%253A%2F%2Figs-buxtehude.de%2Fiserv%2Fapp%2Fauthentication%2Fredirect%26response_type%3Dcode%26scope%3Dopenid%2520uuid%2520iserv%253Asession-id%2520iserv%253Aweb-ui%2520iserv%253A2fa%253Aconfiguration%2520iserv%253Aaccess-groups%26state%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjEifQ.eyJyZWRpcmVjdF91cmkiOiJodHRwczpcL1wvaWdzLWJ1eHRlaHVkZS5kZVwvaXNlcnZcLyIsIm5vbmNlIjoiMzM0YTY4YmUtMzkwMC00MzA0LWFlMWQtYWQ2YTk3ZGU0MjBkIiwiYWRtaW4iOmZhbHNlLCJpc3MiOiJodHRwczpcL1wvaWdzLWJ1eHRlaHVkZS5kZVwvaXNlcnZcLyIsImV4cCI6MTY2OTIyOTA2OSwibmJmIjoxNjY5MTQyNjA5LCJpYXQiOjE2NjkxNDI2NjksInNpZCI6IiJ9.5lYYvDxPn7foBhGRwzKatK9IHbRG1jntIwQuue96c5WH8ZJxMmqgmDbOU0I-jK6a0pLWyzAacmyGko4s4TNz-g",
@@ -144,35 +149,77 @@ async def notify(rows, day, date):
             Type = "Raumänderung"
 
         if Type == "Unterrichtsänderung":
-            print(f'{Day} gibt es eine {Type} in der {Hour} Stunde: {Subject}')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} gibt es eine {Type} in der {Hour} Stunde: {Subject}')
-                webhook.execute()
+            message = f'{Day} gibt es eine {Type} in der {Hour} Stunde: {Subject}'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
         elif Type == "Betreuung":
-            print(f'{Day} in der {Hour} Stunde habt ihr {Type} von {Teacherchange} in {Subject}.')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} in der {Hour} Stunde habt ihr {Type} von {Teacherchange} in {Subject}.')
-                webhook.execute()
+            message = f'{Day} in der {Hour} Stunde habt ihr {Type} von {Teacherchange} in {Subject}.'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
         elif Type == "Ausfall" or Type == "Entfall":
-            print(f'{Day} in der {Hour} Stunde habt ihr {Type} in {Subject}.')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} in der {Hour} Stunde habt ihr {Type} in {Subject}.')
-                webhook.execute()
+            message = f'{Day} in der {Hour} Stunde habt ihr {Type} in {Subject}.'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
         elif Type == "Raumänderung":
-            print(f'{Day} in der {Hour} Stunde habt ihr eine {Type} in {Subject}: {Room}')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} in der {Hour} Stunde habt ihr eine {Type} in {Subject}: {Room}')
-                webhook.execute()
+            message = f'{Day} in der {Hour} Stunde habt ihr eine {Type} in {Subject}: {Room}'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
         elif Type == "Pausenaufsicht":
-            print(f'{Day} in der {Hour} Stunde hat euer Lehrer {Teacher} {Type}')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} in der {Hour} Stunde hat {Teacher} {Type}')
-                webhook.execute()
+            message = f'{Day} in der {Hour} Stunde hat euer Lehrer {Teacher} {Type}'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
         else:
-            print(f'{Day} in der {Hour} Stunde habt ihr {Type}')
-            if notify_method == "discord_webhook":
-                webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=f'{Day} in der {Hour} Stunde habt ihr {Type} in {Subject}')
-                webhook.execute()
+            message = f'{Day} in der {Hour} Stunde habt ihr {Type}'
+            print(message)
+            for method in notify_method:
+                if method.lower() == "discord_webhook":
+                    webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, content=message)
+                    webhook.execute()
+                elif method.lower() == "matrix_notifier":
+                    try:
+                        requests.post(url, data=message)
+                    except requests.exceptions.RequestException:
+                        print("Matrix-Nofier seems to be down")
 
 
 async def save(table):
